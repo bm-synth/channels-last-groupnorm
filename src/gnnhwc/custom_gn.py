53f3bf8ad645ba09985f85a13cf53710d17ab7e1
Ivan Chelombiev <ivan.chelombiev@synthesia.io>
2025-03-10 16:17:33 +0000

    ruff

--- src/gnnhwc/custom_gn.py
+++ src/gnnhwc/custom_gn.py
@@ -2,11 +2,11 @@ import os
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional as F  # noqa
 
 module_dir = os.path.dirname(os.path.abspath(__file__))
 
-from torch.utils.cpp_extension import load
+from torch.utils.cpp_extension import load  # noqa
 
 gn_op = load(
     name="gn_op",
@@ -31,28 +31,28 @@ gn_op = load(
 )
 
 
-class GN_NHWC_Func(torch.autograd.Function):
+class GN_NHWC_Func(torch.autograd.Function):  # noqa
     @staticmethod
-    def forward(ctx, X: torch.Tensor, weight: torch.Tensor, bias: torch.Tensor, G: int, eps: float, activation: str):
-        ctx.x_shape = X.shape
+    def forward(ctx, x: torch.Tensor, weight: torch.Tensor, bias: torch.Tensor, g: int, eps: float, activation: str):
+        ctx.x_shape = x.shape
 
-        X_flat = X.view(X.shape[0], X.shape[1], -1)
-        X_out, means, rstds = torch.ops.gnop.fwd(X_flat, weight, bias, G, eps, activation)
-        ctx.save_for_backward(X_flat, weight, bias, means, rstds)
-        ctx.G = G
+        x_flat = x.view(x.shape[0], x.shape[1], -1)
+        x_out, means, rstds = torch.ops.gnop.fwd(x_flat, weight, bias, g, eps, activation)
+        ctx.save_for_backward(x_flat, weight, bias, means, rstds)
+        ctx.G = g
         ctx.activation = activation
-        return X_out.view(ctx.x_shape)
+        return x_out.view(ctx.x_shape)
 
     @staticmethod
     def backward(ctx, dy: torch.Tensor):
-        X_flat, weight, bias, means, rstds = ctx.saved_tensors
-        dy = dy.contiguous(memory_format=torch.channels_last).view(X_flat.shape)
-        assert dy.stride() == X_flat.stride()
-        dx, dgamma, dbeta = torch.ops.gnop.bwd(dy, X_flat, weight, bias, means, rstds, ctx.G, ctx.activation)
+        x_flat, weight, bias, means, rstds = ctx.saved_tensors
+        dy = dy.contiguous(memory_format=torch.channels_last).view(x_flat.shape)
+        assert dy.stride() == x_flat.stride()
+        dx, dgamma, dbeta = torch.ops.gnop.bwd(dy, x_flat, weight, bias, means, rstds, ctx.G, ctx.activation)
         return dx.view(ctx.x_shape), dgamma, dbeta, None, None, None
 
 
-class GN_NHWC(nn.GroupNorm):
+class GN_NHWC(nn.GroupNorm):  # noqa
     def __init__(self, num_groups: int, num_channels: int, activation="identity", **kwargs):
         super().__init__(num_groups, num_channels, **kwargs)
         activation_to_code = {
@@ -70,9 +70,10 @@ class GN_NHWC(nn.GroupNorm):
     def forward(self, x):
         # N, C, H, W = x.shape
         # x = x.view(x.shape[0], x.shape[1], -1)
-        G = self.num_groups
+        _ = self.num_groups
         if x.stride(1) == 1:  # channels last format
-            # make sure the other dims in x are contiguous (e.g. shape (2, 3, 5, 9) should have stride (135, 1, 27, 3) and not (135, 1, 3, 15))
+            # make sure the other dims in x are contiguous (e.g. shape (2, 3, 5, 9)
+            # should have stride (135, 1, 27, 3) and not (135, 1, 3, 15))
             inner_dims = range(2, x.ndim)
             x_contiguous = x.permute(0, *inner_dims, 1).contiguous()
             inner_dims = range(1, x.ndim - 1)
@@ -82,7 +83,7 @@ class GN_NHWC(nn.GroupNorm):
             x = x.contiguous()
             activations = [lambda x: x, F.relu, F.silu, F.gelu, lambda x: F.gelu(x, approximate="tanh")]
             act_fn = activations[self.act_code]
-            fwd_fn = lambda x, w, b, g, eps, _act: act_fn(F.group_norm(x, g, w, b, eps))
+            fwd_fn = lambda x, w, b, g, eps, _act: act_fn(F.group_norm(x, g, w, b, eps))  # noqa
 
         if self.affine:
             return fwd_fn(x, self.weight, self.bias, self.num_groups, self.eps, self.act_code)
